9b88f54b31b914382b30d459fa411a49c45baed2
everton_goncalves <everton.goncalves.89@gmail.com>
2024-05-29 08:31:34 -0300

    feat: converting a path

--- java-io/src/main/java/com/egon89/path/ConvertingAPath.java
+++ java-io/src/main/java/com/egon89/path/ConvertingAPath.java
@@ -9,22 +9,38 @@ public class ConvertingAPath {
     // if you need to convert the path to a string that can be opened from a browser
     System.out.println(p1.toUri()); // file:///home/logfile.log
 
+    // > toAbsolutePath()
     // the toAbsolutePath() method can be very helpful when processing user-entered file names.
     // you can ensure that your code works consistently regardless of the starting point of the path.
     // the file doesn't need to exist
-    Path p2 = Path.of("temp/file.txt");
+    Path p2 = Path.of("/tmp/file.txt");
     Path p2f = p2.toAbsolutePath();
-    System.out.println(p2f);  // /home/user/temp/file.txt
+    System.out.println(p2f);  // /tmp/file.txt
 
+    // pay attention to the first slash
+    Path fromCurrent = Path.of("logs/debug.log").toAbsolutePath();
+    Path fromRoot = Path.of("/logs/debug.log").toAbsolutePath();
+    System.out.println(fromCurrent); // /home/user/projects/workspace/logs/debug.log
+    System.out.println(fromRoot); // /logs/debug.log
+
+    // > toRealPath()
     // the toRealPath() method returns the real path of an existing file.
     // Example 1: Resolving a relative path
-    Path relativePath = Path.of("temp/existing-file.txt");
+    Path relativePath = Path.of("/tmp/existing-file.txt"); // run touch /tmp/existing-file.txt first
     try {
       Path realPath = relativePath.toRealPath();
-      System.out.println("Real path: " + realPath);
+      System.out.println("Real path: " + realPath); // Real path: /tmp/existing-file.txt
     } catch (IOException e) {
       System.out.println("Error: " + e.getMessage());
-      e.printStackTrace();
+    }
+
+    // Example 2: Resolving a relative path
+    Path relativePath2 = Path.of("/tmp/no-existing-file.txt");
+    try {
+      Path realPath2 = relativePath2.toRealPath();
+      System.out.println("Real path: " + realPath2);
+    } catch (IOException e) {
+      System.out.println("Error: " + e.getMessage()); // Error: /tmp/no-existing-file.txt
     }
   }
 }
