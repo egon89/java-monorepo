0b83b3afa685975198b6fc777f4231586c90fc3d
everton_goncalves <everton.goncalves.89@gmail.com>
2024-06-14 08:58:24 -0300

    docs: file and file store attributes

---
+++ java-io/src/main/java/com/egon89/managingfilesattributes/FileAndFileStoreAttributes.java
@@ -0,0 +1,8 @@
+package com.egon89.managingfilesattributes;
+
+/**
+ * <a href="https://dev.java/learn/java-io/file-system/metadata/#file-attributes">dev.java</a>
+ */
+public class FileAndFileStoreAttributes {
+  public static void main(String[] args) {}
+}
