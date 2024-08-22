22aa2bc1b75d4e188390b040354f57577780e363
everton_goncalves <everton.goncalves.89@gmail.com>
2024-08-22 08:52:05 -0300

    feat: instanceof pattern matching for object equals

--- pattern-matching/src/main/java/com/egon89/PatternMatchingForInstanceOf.java
+++ pattern-matching/src/main/java/com/egon89/PatternMatchingForInstanceOf.java
@@ -1,5 +1,7 @@
 package com.egon89;
 
+import java.util.Objects;
+
 /**
  * <a href="https://dev.java/learn/pattern-matching/#instanceof">dev.java</a>
  */
@@ -8,6 +10,13 @@ public class PatternMatchingForInstanceOf {
     final var m = new MatchingAnyObjectToATypeWithInstanceOf();
     m.print("John");
     m.print(30);
+
+    final var p1 = new Point(1, 2);
+    final var p2 = new Point(1.01, 2.02);
+    final var p3 = new Point(1, 2);
+
+    System.out.println(p1.equals(p2)); // false
+    System.out.println(p1.equals(p3)); // true
   }
 
   private static class MatchingAnyObjectToATypeWithInstanceOf {
@@ -17,6 +26,19 @@ public class PatternMatchingForInstanceOf {
       } else {
         System.out.println("This is not a String");
       }
+    }
+  }
+
+  private record Point(double x, double y) {
+    @Override
+    public boolean equals(Object o) {
+      return o instanceof Point p
+          && Double.compare(x, p.x) == 0 && Double.compare(y, p.y) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+      return Objects.hash(x, y);
     }
   }
 }
