30464facf6ce5ebafb71db6b688bf0774d170e78
everton_goncalves <everton.goncalves.89@gmail.com>
2024-08-27 09:51:36 -0300

    feat: record pattern matching - switch

--- pattern-matching/src/main/java/com/egon89/RecordPatternMatching.java
+++ pattern-matching/src/main/java/com/egon89/RecordPatternMatching.java
@@ -8,6 +8,11 @@ import java.util.List;
  */
 public class RecordPatternMatching {
   public static void main(String[] args) {
+    usingInstanceOf();
+    usingSwitch();
+  }
+
+  private static void usingInstanceOf() {
     final List<Object> list = List.of(
         "2", new Point(1, 2), new BigDecimal(10));
     list.forEach(o -> {
@@ -23,7 +28,7 @@ public class RecordPatternMatching {
       Not a point instance
     */
 
-     Object o = new Point(0, 1);
+    Object o = new Point(0, 1);
 
     // this doesn't compile
     // we need to use the canonical constructor as record pattern
@@ -33,7 +38,34 @@ public class RecordPatternMatching {
     if (o instanceof Point(var x, var y)) {
       System.out.println("Point instance - type inference");
     }
+  }
 
+  private static void usingSwitch() {
+    final var list = List.of(
+        "2", new Box("2"), new Box(new BigDecimal(10)), new Box(1));
+    list.forEach(o -> {
+      switch(o) {
+        case Box(String s) -> System.out.printf("Box contains the string %s%n", s);
+        case Box(Integer i) -> System.out.printf("Box contains the integer %s%n", i);
+        default -> System.out.println("Box contains something else");
+      }
+    });
+    /*
+      Box contains something else
+      Box contains the string 2
+      Box contains something else
+      Box contains the integer 1
+     */
+
+    final var list2 = List.of(new BoxCS("a"), new BoxCS(new StringBuilder("b")));
+    list2.forEach(o -> {
+      switch(o) {
+        case BoxCS(StringBuilder sb) -> System.out.printf("It contains a string builder %s%n", sb);
+        case BoxCS(String s) -> System.out.printf("It contains a string %s%n", s);
+        // case BoxCS(Integer i) -> System.out.println("compile error!"); // Compile error! Integer is not a CharSequence
+        default -> System.out.println("It contains something else");
+      }
+    });
   }
 
   private record Point(int x, int y) {
@@ -41,4 +73,8 @@ public class RecordPatternMatching {
       this(x, 0);
     }
   }
+
+  private record Box(Object o) {}
+
+  private record BoxCS(CharSequence cs) {}
 }
