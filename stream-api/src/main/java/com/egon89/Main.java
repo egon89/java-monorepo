80ba6318266d256467b2fd62edbc9afd8c3a6c1b
everton_goncalves <everton.goncalves.89@gmail.com>
2024-09-04 18:12:28 -0300

    feat: grouping by string length

--- stream-api/src/main/java/com/egon89/Main.java
+++ stream-api/src/main/java/com/egon89/Main.java
@@ -1,7 +1,23 @@
 package com.egon89;
 
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class Main {
   public static void main(String[] args) {
     System.out.println("Stream API module");
+
+    List<String> strings = List.of("one", "two", "three", "four");
+    // grouping by string length
+    Map<Integer, Long> map = strings.stream()
+        .collect(Collectors.groupingBy(String::length, Collectors.counting()));
+
+    map.forEach((key, value) -> System.out.printf("%d :: %d%n", key, value));
+    /*
+      3 :: 2
+      4 :: 1
+      5 :: 1
+    */
   }
 }
